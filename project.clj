5315dabb787c4a7f72d3bbc4a28980ca03b4d33f
Slobodan Stanic <slobodan.stanic@sujuwa.fi>
2017-01-21 22:41:02 +0200

    moved libs out of dev

--- project.clj
+++ project.clj
@@ -5,22 +5,20 @@
   ; with "lein ring server". It also allows us to package up our application
   ; as a standalone .jar or as a .war for deployment to a servlet contianer
   ; (I know... SO 2005).
-  :plugins [[lein-ring "0.10.0"]]
+  :plugins [[lein-ring "0.10.0"]
+            [lein-heroku "0.5.3"]]
 
   ; See https://github.com/weavejester/lein-ring#web-server-options for the
   ; various options available for the lein-ring plugin
   :ring {:handler restful-clojure.handler/app
          :nrepl {:start? true
                  :port 9998}}
-                 
-   :min-lein-version "2.0.0"
 
-  :profiles
-  {:dev {:dependencies [
-                        [org.clojure/clojure "1.7.0"]
-                        [ring/ring-core "1.2.1"]
-                        [ring/ring-jetty-adapter "1.2.1"]
-                        [compojure "1.1.6"]
-                        [ring/ring-json "0.4.0"]
-                        [javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                [ring/ring-core "1.2.1"]
+                [ring/ring-jetty-adapter "1.2.1"]
+                [compojure "1.1.6"]
+                [ring/ring-json "0.4.0"]
+                [javax.servlet/servlet-api "2.5"]
+                [ring-mock "0.1.5"]])
